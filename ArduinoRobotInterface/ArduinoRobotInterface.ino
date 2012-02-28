7b5ffad2e0eedde73df6502502cc6c15a4645a14
Dan <dan@9thsense.com>
2012-02-28 01:05:53 -0500

    fully working version, including ardruino/freeduino app, use this copy

--- ArduinoRobotInterface/ArduinoRobotInterface.ino
+++ ArduinoRobotInterface/ArduinoRobotInterface.ino
@@ -18,26 +18,19 @@ void setup()
  
 void loop()
 {
-  byte msg[0]; // one byte
-  int value=10; // value to send,we'll increment and decrement this variable
+  byte msg[128]; 
+  //int value=10; // value to send,we'll increment and decrement this variable
   if (acc.isConnected())
   {
       // is connected
-      int len = acc.read(msg, sizeof(msg), 1); // read data into msg variable
+      int len = acc.read(msg, sizeof(msg), 128); // read data into msg variable
       if (len > 0) {
-        if (msg[0] == 1) // compare received data
+        if (msg[0] == 'f') // compare received data
           digitalWrite(LED_PIN,HIGH); // turn on light
         else
           digitalWrite(LED_PIN,LOW); // turn off light
-      }
-     
-      while(value>0)
-      {
-          // count down
-          msg[0] = value;
-          acc.write(msg, 1);
-          delay(500);
-          value-=1;
-      }
+        acc.write(msg, len); // echo the message'
+      }     
   }
+  delay(10);
 }
